4d217d22932a22711108363b0a591020763f0001
Joe Riggs <jriggs@safenet-inc.com>
2015-06-24 07:12:32 -0700

    Cleaned socket test programs.  Ready to implement.

--- c_progs/sockets/socket_client.c
+++ c_progs/sockets/socket_client.c
@@ -7,41 +7,13 @@ int main(int argc, char **argv)
 
   printf("Socket Client\n");
 
-  int fd = shm_open(SHM_PATH, O_RDWR, 0666);
-  printf("shm_open() returned %d (%s).\n", fd,
-            (fd >= 0) ? "PASS": "FAIL");
-  if(fd >= 0)
+  int sock = socket(AF_INET,SOCK_STREAM, 0);
+  printf("socket() returned %d (%s).\n", sock, (sock >= 0) ? "PASS": "FAIL");
+  if(sock >= 0)
   {
-    unsigned char *m = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if(m != MAP_FAILED)
-    {
-      printf("mmap() returned %p (%s).\n", m, "PASS");
 
-      /* Spin real slowly, watching the memory space. */
-      unsigned char c = 0;
-      while(c != 0xFF)
-      {
-        if(*m != c)
-        {
-          printf("New value (%x != %x).\n", *m, c);
-          c = *m;
-        }
-
-        sleep(1);
-      }
-
-      retval = munmap(m, SHM_SIZE);
-      printf("unmap() returned %d (%s).\n", retval, (retval == 0) ? "PASS" : "FAIL");
-    }
-    else
-    {
-      char *err = strerror(errno);
-      printf("mmap() returned %p (%s).\n", m, err);
-    }
-
-    retval = shm_unlink(SHM_PATH);
-    printf("shm_unlink() returned %d (%s).\n", retval,
-            (retval == 0) ? "PASS" : "FAIL");
+    retval = close(sock);
+    printf("close() returned %d (%s).\n", retval, (retval == 0) ? "PASS" : "FAIL");
   }
 
   return 0;
--- c_progs/sockets/socket_test.h
+++ c_progs/sockets/socket_test.h
@@ -2,16 +2,9 @@
 #ifndef __SOCKET_TEST_H__
 #define __SOCKET_TEST_H__
 
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 
-#include <sys/mman.h>
-#include <sys/stat.h>
-
-#define SHM_PATH "my_shm"
-#define SHM_SIZE 65536
+#include <sys/socket.h>
 
 #endif // __SOCKET_TEST_H__
