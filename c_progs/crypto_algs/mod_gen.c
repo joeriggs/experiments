9fced656d95989be23f3c0733505368bb1cdd771
Joe Riggs <joe.riggs.1962@gmail.com>
2016-01-30 10:21:31 -0500

    Modified mod_gen to allow multiple sets of test data.
    Moved the test data to an array and put the test code into a for() loop.

--- c_progs/crypto_algs/mod_gen.c
+++ c_progs/crypto_algs/mod_gen.c
@@ -21,13 +21,14 @@
  ******************************************************************************/
 static int64_t do_exponentiation(int64_t base, int64_t exp)
 {
-  int64_t result = 1;
+	int64_t result = 1;
 
-  while(exp-- > 0) {
-    result *= base;
-  }
+	while(exp-- > 0)
+	{
+		result *= base;
+	}
 
-  return result;
+	return result;
 }
 
 /*******************************************************************************
@@ -35,55 +36,79 @@ static int64_t do_exponentiation(int64_t base, int64_t exp)
  ******************************************************************************/
 int mod_gen_test(void)
 {
-  int rc = 0;
+	printf("Testing generator and modulus.\n");
 
-  int64_t mod = 19;
-  int64_t gen =  3;
+	int rc = 0;
 
-  printf("Testing generator and modulus.\n");
+	typedef struct test_data {
+		int64_t mod;
+		int64_t gen;
+	} test_data;
+	test_data tests[] = {
+		{ 19, 3 },
+		{ 10, 2 }
+	};
+	int num_tests = sizeof(tests) / sizeof(test_data);
 
-  int list_size = (sizeof(gen) * mod);
-  int64_t *list = (int64_t *) malloc(list_size);
-  if(list == (int64_t *) 0) {
-    printf("Unable to allocate buffer.\n");
-    rc = 1;
-  }
-  memset(list, 0, list_size);
+	int i;
+	for(i = 0; i < num_tests; i++)
+	{
+		int64_t mod = tests[i].mod;
+		int64_t gen = tests[i].gen;
 
-  if(rc == 0) {
-    int64_t i;
-    for(i = 0; (rc == 0) && (i < mod); i++) {
-      int64_t res = do_exponentiation(gen, i) % mod;
-      if(res >= mod) {
-        printf("Result looks weird.\n");
-        rc = 2;
-      }
-      else if(list[res] != 0) {
-        printf("Multiple exponents got the same result.\n");
-        rc = 3;
-      }
-      else {
-        list[res] = i;
-      }
-    }
-  }
+		int list_size = (sizeof(gen) * mod);
+		int64_t *list = (int64_t *) malloc(list_size);
+		if(list == (int64_t *) 0)
+		{
+			printf("Unable to allocate buffer.\n");
+			rc = 1;
+		}
+		memset(list, 0, list_size);
 
-  if(rc == 0) {
-    int i;
-    for(i = 0; i < mod; i++) {
-      int x;
-      for(x = 0; x < mod; x++) {
-        if(list[x] == i) {
-          break;
-        }
-      }
+		if(rc == 0)
+		{
+			int64_t i;
+			for(i = 0; (rc == 0) && (i < mod); i++)
+			{
+				int64_t res = do_exponentiation(gen, i) % mod;
+				if(res >= mod)
+				{
+					printf("Result looks weird.\n");
+					rc = 2;
+				}
+				else if(list[res] != 0)
+				{
+					printf("Multiple exponents got the same result.\n");
+					rc = 3;
+				}
+				else
+				{
+					list[res] = i;
+				}
+			}
+		}
 
-      printf("%3jd = (%3jd ^ %3d) mod %3jd :: %3d = (%3jd ^ %3d) mod %3jd\n",
-             list[i], gen, i, mod,
-             i, gen, x, mod);
-    }
-  }
+		if(rc == 0)
+		{
+			int i;
+			for(i = 0; i < mod; i++)
+			{
+				int x;
+				for(x = 0; x < mod; x++)
+				{
+					if(list[x] == i)
+					{
+						break;
+					}
+				}
 
-  return rc;
+				printf("%3jd = (%3jd ^ %3d) mod %3jd :: %3d = (%3jd ^ %3d) mod %3jd\n",
+				       list[i], gen, i, mod,
+				       i, gen, x, mod);
+			}
+		}
+	}
+
+	return rc;
 }
 
