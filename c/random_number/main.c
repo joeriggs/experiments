41b38d508e03b0778693b55e7ca21b4f358f6fdc
Joe Riggs <joe.riggs.1962@gmail.com>
2017-01-26 20:20:57 -0500

    Added a random_number prog.  Just calls rand().  Nothing original.

---
+++ c/random_number/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int val1 = rand();
+	int val2 = rand();
+	int val3 = rand();
+	int val4 = rand();
+	printf("%d %d %d %d.\n", val1, val2, val3, val4);
+
+	return 0;
+}
+
