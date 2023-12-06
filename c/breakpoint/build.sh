52e06412c420cdd4fd42cc27f65e44e8a23d3075
Joe Riggs <jriggs@ubuntu-20.04-client>
2023-12-06 06:36:22 -0500

    breakpoint project: Got rid of some obsolete files.

--- c/breakpoint/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -O0 -g breakpoint.c -o breakpoint -lrt
-[ $? -ne 0 ] && echo "breakpoint build failed." && exit 1
-
-gcc -O0 -g stack_trace.c -o stack_trace
-[ $? -ne 0 ] && echo "stack_trace build failed." && exit 1
-
-exit 0
